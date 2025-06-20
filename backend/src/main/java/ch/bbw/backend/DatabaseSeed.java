a3a630c4c95ae09f1bf0d60e003f9ba280eaa50b
ere-blinh <adessoguest@outlook.com>
2025-06-20 10:29:43 +0200

    files for backend

--- backend/src/main/java/ch/bbw/backend/DatabaseSeed.java
+++ backend/src/main/java/ch/bbw/backend/DatabaseSeed.java
@@ -1,32 +1,29 @@
-//package ch.bbw.backend;
+package ch.bbw.backend;
 
-//import ch.bbw.backend.user.Role;
-//import ch.bbw.backend.user.User;
-//import ch.bbw.backend.user.UserService;
-//import jakarta.transaction.Transactional;
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.stereotype.Component;
+import ch.bbw.backend.user.User;
+import ch.bbw.backend.user.UserRequestDTO;
+import ch.bbw.backend.user.UserService;
+import jakarta.transaction.Transactional;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
-//@Component
-//public class DatabaseSeed implements CommandLineRunner {
-//  private final UserService userService;
-//
-//  public DatabaseSeed(UserService userService) {
-//      this.userService = userService;
-//  }
-//
-//@Transactional
-//  @Override
-//  public void run(String... args) {
-//      // Nur erstellen, wenn noch nicht vorhanden
-//      if (!userService.existsByEmail("admin@bbcag.com")) {
-//          User user = new User();
-//          user.setEmail("admin@bbcag.com");
-//          user.setPassword("admin1234"); // Achtung: vermutlich musst du hier noch verschlüsseln!
-//          user.setUsername("Admin");
-//          user.setRole(Role.ADMIN);
-//          userService.create(user);
-//      }
+@Component
+public class DatabaseSeed implements CommandLineRunner {
+  private final UserService userService;
 
-//  }
-//}
+  public DatabaseSeed(UserService userService) {
+      this.userService = userService;
+  }
+
+@Transactional
+  @Override
+  public void run(String... args) {
+      // Nur erstellen, wenn noch nicht vorhanden
+      if (!userService.existsByEmail("admin@bbcag.com")) {
+          UserRequestDTO userRequestDTO = new UserRequestDTO();
+          userRequestDTO.setEmail("admin@bbcag.com");
+          userRequestDTO.setPassword("admin1234"); // Achtung: vermutlich musst du hier noch verschlüsseln!
+          userService.createUser(userRequestDTO);
+      }
+  }
+}
